a022c049f6c0814f9313ff6771ece65388f5812f
nguyenson-255 <nguyenson10b12@gmail.com>
2025-03-17 13:42:19 +0700

    ui login and signup

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,10 +1,72 @@
-import React  from 'react';
+import React, { useState }  from 'react';
+import '../css/Login.css'
+import {Link} from "react-router-dom";
+import api from '../api/api'
+import { Button, IconButton, Input, Snackbar } from '../../node_modules/@mui/material/index';
 
 export default function Login() {
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [open, setOpen] = useState(false);
+  const [message, setMessage] = useState('');
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const action = (
+    <React.Fragment>
+      <Button color="secondary" size="small" onClick={handleClose}>
+        UNDO
+      </Button>
+      <IconButton
+        size="small"
+        aria-label="close"
+        color="inherit"
+        onClick={handleClose}
+      >
+      </IconButton>
+    </React.Fragment>
+  );
+
+  const HandleLogin = () => {
+    api.post('/api/users/login', {
+      email: email,
+      password: password
+    })
+    .then(function (response) {
+      console.log(response);
+      
+      setOpen(true);
+      setMessage("Login Success!!");
+      localStorage.setItem("token",  response.data.accessToken);
+    })
+    // .catch(function (e) {
+    //   setOpen(true);      
+    //   setMessage(e[0]);
+    // });
+  }
+
   return (
     <div>
-      <h1>Welcome to Login Page</h1>
-      <p>This is a React class component.</p>
+      <form action={HandleLogin}>
+        <div className='header'>
+          <h5>User Login</h5>
+          <Link to="/signup">Go to register</Link>
+        </div>
+        <Input type='text' title='Email' placeholder='fill your email' value={email} onChange={(e) => {setEmail(e.target.value)}} required></Input>
+        <Input type='password' title='Password' placeholder='fill your password' value={password} onChange={(e) => {setPassword(e.target.value)}} required />
+        <input type="submit" value="Login"/>
+      </form>
+      <Snackbar
+        anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+        open={open}
+        autoHideDuration={2000}
+        onClose={handleClose}
+        message={message}
+        action={action}
+      />
     </div>
   );
 }
--- frontend/src/pages/SignUp.js
+++ frontend/src/pages/SignUp.js
@@ -1,11 +1,67 @@
-import React  from 'react';
+import React, { useState }  from 'react';
+import '../css/SignUp.css'
+import {Link} from "react-router-dom";
+import { Input } from '../../node_modules/@mui/material/index';
+import api from '../api/api'
 
 export default function SignUp() {
+
+  const [field,setField] = useState({
+    email: '',
+    username: '',
+    password: '',
+    passwordConfirm: '',
+  });
+
+
+  const handleChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+
+    setField({
+      ...field,
+      [name] : value
+    });
+  }
+
+  const HandleSignUp = () => {
+
+    console.log(field.email, field.username);
+    
+    api.post('/api/users/', {
+      email: field.email,
+      password: field.password,
+      username: field.username
+    })
+    .then(function (response) {
+      alert('Register Success');
+      console.log(response.data);
+    })
+    .catch(function (error) {
+      console.log(error);
+      
+      alert(error);
+    });
+  }
+
   return (
     <div>
-      <h1>Welcome to SignUp Page</h1>
-      <p>This is a React class component.</p>
+      <form action={HandleSignUp}>
+        <div className='header'>
+          <h5>Register</h5>
+          <Link to="/login">Go to login</Link>          
+        </div>
+        <label htmlFor='email'>Email: </label>
+        <Input id='email' name='email' type='email' title='Email' placeholder='fill your email' onChange={handleChange} required></Input>
+        <label htmlFor='email'>Username: </label>
+        <Input id='username' name='username' type='text' title='Email' placeholder='fill your username' onChange={handleChange} required></Input>
+        <label htmlFor='email'>Password: </label>
+        <Input id='password' name='password' type='password' title='Password' placeholder='fill your password' onChange={handleChange} required></Input>
+        <label htmlFor='email'>Password Confirm: </label>
+        <Input id='passwordConfirm' name='passwordConfirm' type='password' title='PasswordConfirm' placeholder='fill your password confirm' onChange={handleChange} required></Input>
+        <input type="submit" value="Sign Up"/>
+      </form>
     </div>
-  );
+  )
 }
 
