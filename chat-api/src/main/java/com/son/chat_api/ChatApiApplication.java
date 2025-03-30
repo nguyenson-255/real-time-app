e612f3b8ce9b05c0960ef2cecb727f09adc84581
nguyenson-255 <nguyenson10b12@gmail.com>
2025-03-30 15:45:55 +0700

    set up chat-api

---
+++ chat-api/src/main/java/com/son/chat_api/ChatApiApplication.java
@@ -0,0 +1,23 @@
+package com.son.chat_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@RestController
+@SpringBootApplication
+public class ChatApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatApiApplication.class, args);
+	}
+
+	@GetMapping("path")
+	public String getMethodName() {
+		return "alo1111";
+	}
+	
+
+}
